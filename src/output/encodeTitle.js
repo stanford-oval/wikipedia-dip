9b59a8f6250d4f974c89b37192652580a741f0c8
Sina <s.j.semnani@gmail.com>
2024-03-01 20:45:44 +0000

    Fix file names for non-Latin scripts

--- src/output/encodeTitle.js
+++ src/output/encodeTitle.js
@@ -2,17 +2,14 @@
 const encodeTitle = function (title) {
   title = title || ''
   title = title.trim()
-  //titlecase it
-  title = title.charAt(0).toUpperCase() + title.substring(1)
-  //spaces to underscores
-  title = title.replace(/ /g, '_')
-  // escape slashes, or possible absolute paths
-  title = encodeURIComponent(title)
   // clobber any potential dot files, or relative paths
   title = title.replace(/^\./g, '\\./')
-  // some operating systems complain with long filenames
-  if (title.length >= 255) {
-    title = title.substr(0, 254) //truncate it
+  // Allow non-ASCII characters, but replace /, null, and control characters
+  title = title.replace(/[/\x00-\x1F\x7F]/g, '_')
+  // console.log(title)
+  // some operating systems complain with long filenames or path names
+  if (title.length >= 200) {
+    title = title.substr(0, 200) //truncate it
   }
   return title
 }
